d1d4ec5bd8198ba994cd6e32dc483466c1de18fb
Tetsuya-Nishikawa <te1823ya@gmail.com>
2020-03-07 18:10:01 +0900

    io_data.pyからmaskを削除

--- io_data.py
+++ io_data.py
@@ -1,9 +1,14 @@
 import tensorflow as tf
 import skvideo.io
 import imutils
+import glob
+import os
 
-train_path = "./dataset/lsa_train_data.tfrecords"
-test_path  = "./dataset/lsa_test_data.tfrecords"
+#input_path = "/home/ubuntu/nfs/cybozu/Temporal_Transformation/SampledVideo"
+#/home/ubuntu/nfs/cybozu/video_dataset/train_dataset
+#/home/ubuntu/nfs/cybozu/video_dataset/test_dataset
+tfrecord_train_dir = "/home/ubuntu/nfs/cybozu/video_dataset/tfrecords/train.tfrecords"
+tfrecord_test_dir  = "/home/ubuntu/nfs/cybozu/video_dataset/tfrecords/test.tfrecords"
 
 def tensor_cast(inputs, labels, mask):
     inputs = tf.cast(inputs, tf.float32)
@@ -39,7 +44,7 @@ def _int64_feature(value):
     """Returns an int64_list from a bool / enum / int / uint."""
     return tf.train.Feature(int64_list=tf.train.Int64List(value=[value]))
 
-def serialize_example(video, label, video_shape, mask):
+def serialize_example(video, label, video_shape):
     feature = {
         'video': _bytes_feature(video),
         'label': _int64_feature(label),
@@ -47,7 +52,6 @@ def serialize_example(video, label, video_shape, mask):
         'height': _int64_feature(video_shape[1]),
         'width': _int64_feature(video_shape[2]),
         'depth': _int64_feature(video_shape[3]),
-        'mask' : _bytes_feature(mask),
     }
     example_proto = tf.train.Example(features=tf.train.Features(feature=feature))
     return example_proto.SerializeToString()
@@ -60,7 +64,6 @@ def parse_tfrecord(serialized_example):
         'height': tf.io.FixedLenFeature((), tf.int64),
         'width': tf.io.FixedLenFeature((), tf.int64),
         'depth': tf.io.FixedLenFeature((), tf.int64),
-        'mask' : tf.io.FixedLenFeature((), tf.string),
     }
     example = tf.io.parse_single_example(serialized_example, feature_description)
     
@@ -68,13 +71,11 @@ def parse_tfrecord(serialized_example):
     video_shape = [example['len_seq'], example['height'], example['width'], example['depth']]
     video = tf.reshape(video, video_shape)
 
-    mask = tf.io.parse_tensor(example['mask'], out_type = tf.bool)
-
-    return video, example['label'], mask
+    return video, example['label']
 
 def write_dataset():
-    train_input_path = "*.mp4"
-    test_input_path = "*.mp4"
+    train_input_path = "/home/ubuntu/nfs/cybozu/video_dataset/train_dataset/*.mp4"
+    test_input_path = "/home/ubuntu/nfs/cybozu/video_dataset/test_dataset/*.mp4"
 
     with tf.io.TFRecordWriter(tfrecord_train_dir) as writer:
 
@@ -82,16 +83,13 @@ def write_dataset():
 
         for video_path in video_list:
             video = skvideo.io.vread(video_path)
-            mask = GenerateMaskTensor(video) 
 
-            mask = tf.io.serialize_tensor(mask)
             video_bytes = tf.io.serialize_tensor(video)
 
-            label = video_path[0]
+            label = video_path.split(os.sep)[7][4:-8]
             label = int(label)
-            print("labelは、", label)
-           
-            example = serialize_example(video_bytes, label, mask, len(video))
+            print("現在処理しているビデオのインデックスは、", video_list.index(video_path))
+            example = serialize_example(video_bytes, label, video.shape)
             writer.write(example)
     with tf.io.TFRecordWriter(tfrecord_test_dir) as writer:
 
@@ -99,16 +97,13 @@ def write_dataset():
 
         for video_path in video_list:
             video = skvideo.io.vread(video_path)
-            mask = GenerateMaskTensor(video) 
 
-            mask = tf.io.serialize_tensor(mask)
             video_bytes = tf.io.serialize_tensor(video)
 
-            label = video_path[0]
+            label = video_path.split(os.sep)[7][4:-8]
             label = int(label)
-            print("labelは、", label)
-           
-            example = serialize_example(video_bytes, label, mask, len(video))
+            print("現在処理しているビデオのインデックスは、", video_list.index(video_path))
+            example = serialize_example(video_bytes, label, video.shape)
             writer.write(example)
 
 
@@ -123,3 +118,6 @@ def read_dataset(BATCH_SIZE):
     test_dataset =  parsed_test_dataset.map(tensor_cast).shuffle(buffer_size=10,  seed=100).padded_batch(BATCH_SIZE,  padded_shapes=([201, 112, 200, 3], [201, 64], [201]), padding_values=padded_shape).prefetch(tf.data.experimental.AUTOTUNE)
     return train_dataset, test_dataset
 
+#データセットを作成するときだけ、実行する
+if __name__ == '__main__':
+    write_dataset()
