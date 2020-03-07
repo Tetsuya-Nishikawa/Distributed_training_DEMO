883494f116207650eb3a56ef96efde5481368d0b
Tetsuya-Nishikawa <te1823ya@gmail.com>
2020-03-07 14:57:52 +0900

    変更

--- Model.py
+++ Model.py
@@ -12,7 +12,8 @@ class Model(tf.keras.Model):
         self.batch_size = batch_size
         self.epochs = epochs
         self.mirrored_strategy = tf.distribute.MirroredStrategy()
-        
+        self.loss_object  = tf.keras.losses.SparseCategoricalCrossentropy(reduction=tf.keras.losses.Reduction.NONE)
+
         with self.mirrored_strategy.scope():
             self.train_acc = tf.keras.losses.SparseCategoricalCrossentropy()
             self.test_acc  = tf.keras.losses.SparseCategoricalCrossentropy()
@@ -30,74 +31,81 @@ class Model(tf.keras.Model):
         
         self.bn4         = tf.keras.layers.BatchNormalization()
         self.dense3  = tf.keras.layers.TimeDistributed(tf.keras.layers.Dense(64, activation=None))
+        if opt_name=="Adam":
+            self.opt             = tf.keras.optimizers.Adam(alpha)
+        if opt_name=="Sgd":
+            self.opt             = tf.keras.optimizers.SGD(alpha)
 
-        def call(self, inputs, training=None):
-            outputs = inputs
-            outputs = self.flatten1(outputs)
-            outputs = self.dense1(outputs)
+    def call(self, inputs, training=None):
+        outputs = inputs
+        outputs = self.flatten1(outputs)
+        outputs = self.dense1(outputs)
 
-            return outputs
+        return outputs
 
-        def train_step(self, images, labels):
-            with tf.GradientTape() as tape:
-                pred = self(images, True)
-                loss = tf.compat.v1.losses.softmax_cross_entropy(onehot_labels=labels, logits=pred, reduction=tf.compat.v1.losses.Reduction.NONE)
-            grads = tape.gradient(loss, self.trainable_variables)
-            self.opt.apply_gradients(zip(grads, self.trainable_variables))
-            loss = tf.nn.compute_average_loss(loss, global_batch_size=self.batch_size)
-            return loss
+    def train_step(self, videos, labels):
+        with tf.GradientTape() as tape:
+            pred = self(videos, True)
+            #loss = tf.compat.v1.losses.softmax_cross_entropy(onehot_labels=labels, logits=pred, reduction=tf.compat.v1.losses.Reduction.NONE)
+            loss = self.loss_object(labels, pred)
 
-        def test_step(self, images, labels, mask):
-            pred = self(images, mask, False)
-            loss  = tf.compat.v1.losses.softmax_cross_entropy(onehot_labels=labels, logits=pred, reduction=tf.compat.v1.losses.Reduction.NONE)
-            loss =  tf.nn.compute_average_loss(loss, global_batch_size=self.batch_size)
+        grads = tape.gradient(loss, self.trainable_variables)
+        self.opt.apply_gradients(zip(grads, self.trainable_variables))
+        loss = tf.nn.compute_average_loss(loss, global_batch_size=self.batch_size)
+        return loss
 
-            return loss
+    def test_step(self, videos, labels):
+        pred = self(videos, False)
+        #loss  = tf.compat.v1.losses.softmax_cross_entropy(onehot_labels=labels, logits=pred, reduction=tf.compat.v1.losses.Reduction.NONE)
+        loss = self.loss_object(labels, pred)
+        loss =  tf.nn.compute_average_loss(loss, global_batch_size=self.batch_size)
 
-        @tf.function
-        def distributed_train_step(self, images, labels, mask):
-                return  self.mirrored_strategy.experimental_run_v2(self.train_step, args=(images, labels, mask))
+        return loss
 
-        @tf.function
-        def distributed_test_step(self, images, labels, mask):
-                return  self.mirrored_strategy.experimental_run_v2(self.test_step,  args=(images, labels, mask))
+    @tf.function
+    def distributed_train_step(self, videos, labels):
+            return  self.mirrored_strategy.experimental_run_v2(self.train_step, args=(videos, labels))
 
-        def train(self, train_ds, test_ds):
-            with self.mirrored_strategy.scope():
-                for epoch in range(self.epochs):
+    @tf.function
+    def distributed_test_step(self, videos, labels):
+            return  self.mirrored_strategy.experimental_run_v2(self.test_step,  args=(videos, labels))
 
-                    train_mean_loss = 0.0
-                    test_mean_loss = 0.0
-                    num_batches = 0.0
-                    
-                    if (epoch+1)%10==0 :
-                        self.opt.learning_rate = self.opt.learning_rate*0.1
+    def train(self, train_ds, test_ds):
+        with self.mirrored_strategy.scope():
+            for epoch in range(self.epochs):
 
-                    for (batch, (train_images, train_labels, masks)) in enumerate(train_ds): 
-                        print("hparam : ", params[param], "epoch : ", epoch+1, "batch : ",batch+1)
-                        losses            = self.distributed_train_step(train_images, train_labels, masks)
-                        train_mean_loss       = train_mean_loss +mirrored_strategy.reduce(tf.distribute.ReduceOp.SUM,losses,axis=None)
-                        num_batches += 1.0 
+                train_mean_loss = 0.0
+                test_mean_loss = 0.0
+                num_batches = 0.0
                 
-                    train_mean_loss = train_mean_loss / num_batches
-                    print("the number of train batch : ", batch+1, "train_mean_loss : ", train_mean_loss, "train acc : ", train_acc.numpy()*100)
-                    num_batches = 0.0
-                    for (batch, (test_images, test_labels, masks)) in enumerate(test_ds):
-                        losses           = self.distributed_test_step(test_images, test_labels, masks)
-                        test_mean_loss  =  test_mean_loss  + mirrored_strategy.reduce(tf.distribute.ReduceOp.SUM, losses,axis=None)
-                        num_batches += 1.0
-        
-                    #dataplotmanager.y_list.append(train_accuracy.result().numpy()*100)
-                    #dataplotmanager.t_list.append(test_accuracy.result().numpy()*100)
-                    test_mean_loss = test_mean_loss / num_batches
-                    print("the number of test batch : ", batch+1)
-                    print("epoch : ", epoch+1, " | train loss value : ", train_mean_loss.numpy(), ", test loss value : ", test_mean_loss.numpy())
-                    print("train acc : ", train_acc.numpy()*100, "test acc : ", test_acc.numpy()*100)
-                    self.accuracy_reset()
-                    #weights_filename = "Model" + str(a) + str(l) + ".bin"
-                    #self.save_weights(weights_filename)#定期的に保存
+                if (epoch+1)%10==0 :
+                    self.opt.learning_rate = self.opt.learning_rate*0.1
 
-        def accuracy_reset(self):
-            self.train_acc.reset_states()
-            self.test_acc.reset_states()
+                for (batch, (train_videos, train_labels)) in enumerate(train_ds): 
+                    print("hparam : ", "epoch : ", epoch+1, "batch : ",batch+1)
+                    losses            = self.distributed_train_step(train_videos, train_labels)
+                    train_mean_loss       = train_mean_loss +self.mirrored_strategy.reduce(tf.distribute.ReduceOp.SUM,losses,axis=None)
+                    num_batches += 1.0 
+            
+                train_mean_loss = train_mean_loss / num_batches
+                print("the number of train batch : ", batch+1, "train_mean_loss : ", train_mean_loss, "train acc : ", train_acc.numpy()*100)
+                num_batches = 0.0
+                for (batch, (test_videos, test_labels)) in enumerate(test_ds):
+                    losses           = self.distributed_test_step(test_videos, test_labels)
+                    test_mean_loss  =  test_mean_loss  + self.mirrored_strategy.reduce(tf.distribute.ReduceOp.SUM, losses,axis=None)
+                    num_batches += 1.0
     
+                #dataplotmanager.y_list.append(train_accuracy.result().numpy()*100)
+                #dataplotmanager.t_list.append(test_accuracy.result().numpy()*100)
+                test_mean_loss = test_mean_loss / num_batches
+                print("the number of test batch : ", batch+1)
+                print("epoch : ", epoch+1, " | train loss value : ", train_mean_loss.numpy(), ", test loss value : ", test_mean_loss.numpy())
+                print("train acc : ", self.train_acc.numpy()*100, "test acc : ", self.test_acc.numpy()*100)
+                self.accuracy_reset()
+                #weights_filename = "Model" + str(a) + str(l) + ".bin"
+                #self.save_weights(weights_filename)#定期的に保存
+
+    def accuracy_reset(self):
+        self.train_acc.reset_states()
+        self.test_acc.reset_states()
+
--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import MyLibrary
 def tensor_cast(inputs, labels):
     inputs = tf.cast(inputs, tf.float32)
     labels = labels - 1
-    inputs = tf.reshape(inputs, [1, inputs.shape[0], inputs.shape[1], inputs.shape[2]])
+    #inputs = tf.reshape(inputs, [1, inputs.shape[0], inputs.shape[1], inputs.shape[2]])
     return inputs, tf.cast(labels, tf.int64)
 
 gpus = tf.config.experimental.list_physical_devices('GPU')
@@ -36,9 +36,10 @@ if __name__ == '__main__':
         a = hparam_list[hparm_key]["alpha"]
         l = hparam_list[hparm_key]["lambd"]
         model = Model("Adam", a, l, batch_size, epochs)
-
+        print("現在のハイパーパラメータは、", "alpha:", a, "lambda", l)
         #train_dataset, test_dataset = in_data.read_dataset(batch_size)
         train_images, train_labels, test_images, test_labels = MyLibrary.ReadMnistDataset()
+
         #padded_shape = (tf.constant(-1.0, dtype=tf.float32), tf.constant(0, dtype=tf.int64), tf.constant(False, dtype=tf.bool))
         train_images = tf.data.Dataset.from_tensors(train_images)
         train_labels = tf.data.Dataset.from_tensors(train_labels)
@@ -47,8 +48,10 @@ if __name__ == '__main__':
 
         train_dataset = tf.data.Dataset.zip((train_images, train_labels)).map(tensor_cast).shuffle(buffer_size=10, seed=100).prefetch(tf.data.experimental.AUTOTUNE)
         test_dataset  = tf.data.Dataset.zip((test_images, test_labels)).map(tensor_cast).shuffle(buffer_size=10, seed=100).prefetch(tf.data.experimental.AUTOTUNE)
-
+        for data, label in train_dataset:
+          print(data.numpy().shape, label.numpy().shape)
+          break
         train_ds =   model.mirrored_strategy.experimental_distribute_dataset(train_dataset)
         test_ds  =   model.mirrored_strategy.experimental_distribute_dataset(test_dataset)
-
+        
         model.train(train_ds, test_ds)
